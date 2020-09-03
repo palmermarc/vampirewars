fe8b5899e0558c5d7a7f55b6b4854ebb1559392f
Marc Palmer <mapalmer@hbi.com>
2020-09-03 14:36:32 -0500

    Added comments to make the code more readable

--- bundles/bundle-diku/commands/clandisc.js
+++ bundles/bundle-diku/commands/clandisc.js
@@ -30,21 +30,24 @@ module.exports = {
 
     const [discName] = args.split(' ');
 
+    // Check to see if they already have that disc
     if(playerdiscs.includes(discName)) {
       // TODO: Return the discipline powers and actually be useful
       return B.sayAt(player, `You have already mastered the ${discName} discipline.`);
     }
 
+    // If they are trying to learn a disc that doesn't exist, tell them
     if (!possibleDiscs.includes(discName)) {
       B.sayAt(player, `That is not an available option. The possible disciplines are:`);
       return B.sayAt(player, possibleDiscs.join(', '));
     }
 
+    // Don't let them get the disc if they are already at the maximum amount
     if( selectedDiscs.length >= maxDiscs ) {
       return B.sayAt(player, `You cannot learn any more disciplines for now.`);
     }
 
-    // Set the stance then return the messaging
+    // We passed every check we through at the system, give them the disc
     player.setMeta('clandiscs.'+discName, 1);
     return B.sayAt(player, `You have mastered the discipline of ${discName}`);
   }
