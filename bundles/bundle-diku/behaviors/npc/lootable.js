def00427b9feefe7ab71d9e7c6625b0665181e79
Marc Palmer <mapalmer@hbi.com>
2020-09-04 12:07:48 -0500

    Fixed an error where the currency was messed up in sacrifice.destroy

--- bundles/bundle-diku/behaviors/npc/lootable.js
+++ bundles/bundle-diku/behaviors/npc/lootable.js
@@ -24,6 +24,7 @@ module.exports = {
         type: 'CONTAINER',
         metadata: {
           noPickup: true,
+          owner: ''
         },
         maxItems: items.length,
         behaviors: {
