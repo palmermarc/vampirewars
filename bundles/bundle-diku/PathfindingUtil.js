2510d4852560d4e164e99df47c0dee35b81ca403
Marc Palmer <palmermarc@gmail.com>
2020-10-01 20:46:04 -0500

    Just finished hunt - still have a few todos but it's fully working

--- bundles/bundle-diku/PathfindingUtil.js
+++ bundles/bundle-diku/PathfindingUtil.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const srcPath = __dirname + '/../../../src/';
+const { Logger } = require('ranvier');
 
 exports.findPathInContinent = (state, ch, targetRoom) => {
+
   const searchRooms = [...state.RoomManager.rooms.values()].filter(room => {
-    return (
-      room.continentId === ch.continentId
-    )
+    return true;
   });
+
   return findPath(state, ch, targetRoom, searchRooms);
 }
 
@@ -19,10 +20,14 @@ function findPath(state, ch, targetRoom, searchRooms) {
   for (const room of searchRooms) {
     mvCosts[room.entityReference] = Infinity;
   }
+
   mvCosts[startRoom.entityReference] = 0;
 
   const backtrace = {};
   const pq = new PriorityQueue();
+
+  Logger.log('Made it this far...');
+
   // Add the first room to queue, then add its exits, etc.
   pq.enqueue(startRoom);
   while (!pq.isEmpty()) {
@@ -36,7 +41,7 @@ function findPath(state, ch, targetRoom, searchRooms) {
 
       // If cost to get to neighbor room is less than any current path to get there,
       // record that cost to compare later. We start at Inifity for this reason.
-      const cost = mvCosts[current.entityReference] + neighbor.movementCost;
+      const cost = mvCosts[current.entityReference] + 1;
       if (cost < mvCosts[neighbor.entityReference]) {
         mvCosts[neighbor.entityReference] = cost;
         backtrace[neighbor.entityReference] = { tarER: current.entityReference, direction: exit.direction };
@@ -45,20 +50,21 @@ function findPath(state, ch, targetRoom, searchRooms) {
     }
   }
 
-  // No Path found.
-  if (mvCosts[targetRoom.entityReference] === Infinity) {
-    return null;
-  }
+  Logger.log('Made it past the pq stuff');
+
+  Logger.log('We found a path, sweet!');
 
   // Use backtrace to construct the optimal path
   let path = [{ tarER: targetRoom.entityReference, direction: null }];
   let lastStep = { tarER: targetRoom.entityReference };
   while (lastStep.tarER !== startRoom.entityReference) {
-    path.unshift(backtrace[lastStep.tarER])
+    path.unshift(backtrace[lastStep.tarER]);
     lastStep = backtrace[lastStep.tarER];
   }
 
-  console.log('path Length:', path.length, 'roomsbacktrace', Object.keys(backtrace).length, 'moves', mvCosts[targetRoom.entityReference], 'mvCosts', Object.keys(mvCosts).length, 'count', pq.count)
+  Logger.log('We should be getting close...');
+
+  Logger.log('path Length:', path.length, 'roomsbacktrace', Object.keys(backtrace).length, 'moves', mvCosts[targetRoom.entityReference], 'mvCosts', Object.keys(mvCosts).length, 'count', pq.count)
   return path;
 };
 
--- bundles/bundle-diku/commands/hunt.js
+++ bundles/bundle-diku/commands/hunt.js
@@ -1,18 +1,39 @@
 'use strict';
 
-const { Broadcast: B } = require('ranvier');
-const Path = require('../PathfindingUtil')
+const { Broadcast: B, Logger } = require('ranvier');
+const Path = require('../PathfindingUtil');
+const Parser = require('..//lib/ArgParser');
+
+function gotoRoom(state, targetMob, targetRoom, player) {
+  if (targetRoom === player.room) {
+    return B.sayAt(player, `You're already there.`);
+  }
+
+  const path = Path.findPathInContinent(state, player, targetRoom);
+
+  if (!path) {
+    return B.sayAt(player, `Can't find a path to ${targetRoom.name}.`);
+  }
+
+  Logger.log(path);
+
+  return B.sayAt(player, `<yellow>${targetMob.name} is ${path[0].direction} from here.</yellow>`);
+
+  //const dirs = path.map(p => p.direction).filter(Boolean);
+  //const parsedDirs = dirs.map(dir => dir[0]).join('');
+  //player.socket.emit('queue-command', player, 'speedwalk ' + parsedDirs);
+}
 
 module.exports = {
-  usage: 'autostance [stance name]',
-  command: (state) => (target, player) => {
-    if (!target.length) {
+  usage: 'hunt [target]',
+  command: (state) => (args, player) => {
+    if (!args.length) {
       return B.sayAt(player, `Whom are you trying to hunt?`);
     }
 
-    if( target == "clear" ) {
-      player.setMeta( 'isHunting', 0 );
-      return B.sayAt( player, `Your hunt target has been cleared.` );
+    if (args == "clear") {
+      player.setMeta('isHunting', 0);
+      return B.sayAt(player, `Your hunt target has been cleared.`);
     }
 
     if (player.isInCombat()) {
@@ -49,44 +70,25 @@ module.exports = {
       }
 
     } else {
+      // Don't allow players to be hunted for now
+      const { area } = player.room;
+
       //let target = Parser.parseDot(dest, player.visiblePlayersOnline) || Parser.parseDot(dest, state.MobManager.getMobs());
-      let target = Parser.parseDot(Parser.parseDot(dest, state.MobManager.getMobs());
+      const target = Parser.parseDot(args, area.npcs);
 
       if (!target) {
-        return B.sayAt(player, 'No such player or mob.');
+        return B.sayAt(player, 'No such mob.');
       }
 
       targetRoom = target.room;
+
+      if (!targetRoom) {
+        return B.sayAt(player, 'No such room exists.');
+      }
+
+      player.setMeta('isHunting', 1);
+
+      gotoRoom(state, target, targetRoom, player);
     }
-
-    if (!targetRoom) {
-      return B.sayAt(player, 'No such room exists.');
-    }
-
-    player.setMeta('isHunting', 1);
-
-    gotoRoom(state, targetRoom, player);
-  },
-  function gotoRoom(state, targetRoom, player) {
-    if (targetRoom === player.room) {
-      return B.sayAt(player, `You're already there.`);
-    }
-
-    if (targetRoom.area.continentId !== player.continentId) {
-      const pCont = state.ContinentManager.getContinent(player.continentId);
-      const roomCont = state.ContinentManager.getContinent(targetRoom.continentId);
-      return B.sayAt(player, `You can't find a path from ${pCont.name} to ${roomCont.name}!`);
-    }
-
-    const path = Path.findPathInContinent(state, player, targetRoom);
-
-    if (!path) {
-      return B.sayAt(player, `Can't find a path to ${targetRoom.name}.`);
-    }
-
-    const dirs = path.map(p => p.direction).filter(Boolean);
-
-    const parsedDirs = dirs.map(dir => dir[0]).join('');
-    player.socket.emit('queue-command', player, 'speedwalk ' + parsedDirs)
   }
 };
