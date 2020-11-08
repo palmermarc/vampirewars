bfdaac6d0e618c9ac9245efe28844d5c87ae21b9
Marc Palmer <palmermarc@gmail.com>
2020-11-08 14:43:36 -0600

    Fixed the "Take me home" script

--- bundles/bundle-diku/areas/midgaard/scripts/rooms/prehistoriaportal.js
+++ bundles/bundle-diku/areas/midgaard/scripts/rooms/prehistoriaportal.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { Broadcast: B} = require('ranvier');
+const { Broadcast: B, Logger} = require('ranvier');
 
 module.exports = {
 	listeners: {
@@ -9,17 +9,20 @@ module.exports = {
 				return;
 			}
 
+			Logger.log(message);
+
 			if (!message.toLowerCase().match('take me home')) {
 				return;
 			}
 
 			if(sender.level > 35) {
+				Logger.log(`${sender.name} is too high of level for the newbie zone!`);
 				return B.sayAt(sender, `You are NOT a newbie and therefore cannot access that area!`);
 			}
 
 			let room = state.RoomManager.getRoom('prehistoria:r11200');
 
-			sender.moveTo(home, () => {
+			sender.moveTo(room, () => {
 				B.sayAt(sender, `A portal opens before you and pulls you in.`);
 				B.sayAtExcept(sender.room, `${sender.name} disappears.`, sender);
 				state.CommandManager.get('look').execute('', sender);
